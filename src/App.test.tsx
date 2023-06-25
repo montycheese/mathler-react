a060d2391cc9e58d9159784b6ba3aadfabf67954
Montana Wong <montanawong@gmail.com>
2023-06-24 17:18:29 -0700

    Add snapshot tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders successfully', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(<App/>)
 });
