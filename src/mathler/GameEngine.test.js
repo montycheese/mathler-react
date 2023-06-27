98075843c6fe881b33d3f1f835d5414f72725392
Montana Wong <montanawong@gmail.com>
2023-06-27 00:26:10 -0700

    Mock mathler client in GameEngine test

--- src/mathler/GameEngine.test.js
+++ src/mathler/GameEngine.test.js
@@ -1,24 +1,38 @@
 import GameEngine from './GameEngine';
+import MathlerClient from "../clients/MathlerClient";
 
+let mockedMathlerClient;
+
+const MOCK_EQUATION = '1+22+3';
+const MOCK_VALUE = 26;
+
+beforeEach(() => {
+    mockedMathlerClient = new MathlerClient('http://example.com/api');
+    mockedMathlerClient.fetchLatestGameData = jest.fn()
+        .mockResolvedValue({ equation: MOCK_EQUATION, value: MOCK_VALUE });
+});
 
 test('It can create a new Mathler game instance', () => {
-    const gameEngine = new GameEngine();
+
+    const gameEngine = new GameEngine(mockedMathlerClient);
     expect(gameEngine.currentInstance).toBeNull();
-    const gameInstance = gameEngine.startNewGameInstance();
-    expect(gameInstance).not.toBeNull();
+
+    return gameEngine.startNewGameInstance().then(instance => {
+        expect(instance).not.toBeNull();
+        expect(instance.value).toEqual(MOCK_VALUE);
+        expect(instance.calculation).toEqual(MOCK_EQUATION);
+    });
 });
 
-test('Get game instance creates a new instance if none are active', () => {
-    const gameEngine = new GameEngine();
-    expect(gameEngine.currentInstance).toBeNull();
-    const gameInstance = gameEngine.getGameInstance();
-    expect(gameInstance).not.toBeNull();
+test('Creating new game instance, fetches data from external API', () => {
+
+    const gameEngine = new GameEngine(mockedMathlerClient);
+
+    return gameEngine.startNewGameInstance().then(() => {
+        expect(mockedMathlerClient.fetchLatestGameData).toHaveBeenCalledTimes(1);
+    });
 });
 
-test('Get game instance returns the current instance', () => {
-    const gameEngine = new GameEngine();
-    const gameInstance = gameEngine.startNewGameInstance();
-    expect(gameInstance).not.toBeNull();
-    const fetchedInstance = gameEngine.getGameInstance();
-    expect(gameInstance).toEqual(fetchedInstance);
-});
+
+
+
