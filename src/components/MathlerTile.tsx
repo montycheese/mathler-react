418bd93270cece08f885c6662dd93b230555116b
Montana Wong <montanawong@gmail.com>
2023-06-27 19:27:08 -0700

    Add small animation to tiles if user guesses a tile correctly

--- src/components/MathlerTile.tsx
+++ src/components/MathlerTile.tsx
@@ -1,4 +1,6 @@
 import {MathlerTileState} from "../mathler/Constants";
+import {useEffect, useRef} from "react";
+import './MathlerTile.css'
 
 type MathlerTileProps = {
     input: string;
@@ -7,10 +9,20 @@ type MathlerTileProps = {
 
 export default function MathlerTile({ input, state }: MathlerTileProps) {
 
+    const prevTileStateRef = useRef<MathlerTileState>(MathlerTileState.NO_VALUE);
+
+    useEffect(() => {
+        prevTileStateRef.current = state;
+    }, [state]);
+
     let bkgColorClass = 'bg-white';
+    let animationClasses = '';
     switch (state) {
         case MathlerTileState.CORRECT_PLACE:
             bkgColorClass = 'bg-green-500';
+            if (prevTileStateRef.current !== MathlerTileState.CORRECT_PLACE) {
+                animationClasses += 'zoom';
+            }
             break;
         case MathlerTileState.DIFFERENT_PLACE:
             bkgColorClass = 'bg-orange-500';
@@ -21,7 +33,7 @@ export default function MathlerTile({ input, state }: MathlerTileProps) {
     }
 
     return (
-        <div className={`${bkgColorClass} rounded-md text-black grid place-items-center p-2 w-10 h-10 hover:bg-cyan-100`}>
+        <div className={`${bkgColorClass} ${animationClasses} rounded-md text-black grid place-items-center p-2 w-10 h-10 hover:bg-cyan-100`}>
             {input}
         </div>
     );
